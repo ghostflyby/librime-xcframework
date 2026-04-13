8362727ea10bfc1fc3a70c2f735c812b3da63bef
ghostflyby <ghostflyby+git@outlook.com>
2026-04-14 00:28:55 +0800

    refactor: reorder includes and define RIME_FLAVORED macro

--- include/RimeShim.h
+++ include/RimeShim.h
@@ -1,6 +1,8 @@
 #pragma once
 
-#include "rime_api.h"
 #include "rime_api_stdbool.h"
-#include "rime_levers_api.h"
 
+#define RIME_FLAVORED(name) name
+
+#include "rime_api.h"
+#include "rime_levers_api.h"
